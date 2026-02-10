c77f9b3aa52c705b7cf50cf1d1897ba66c7fde01
Tony Yanick <yanick.tony@gmail.com>
2026-02-10 17:21:56 -0500

    Fix import route fallback and sync method compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./app/App";
 import "./styles/theme.css";
 import "./styles/components.css";
 
-const baseUrl = import.meta.env.BASE_URL.replace(/\/$/, "");
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={baseUrl}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
